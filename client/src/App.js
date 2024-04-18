ab502a5dc0549bb80fc1cce60eca73518182ce72
zainabbas@123 <zainabbaskhakhi123@gmail.com>
2024-04-18 15:54:22 +0500

    changes

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [data, setData] = React.useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/api").then(async (res) => {
+      const data = await res.json();
+      console.log("data", data);
+      setData(data.message);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{!data ? "Loading..." : data}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
