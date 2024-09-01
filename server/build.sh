cf9bf088deeb8ef862224831f2f14437fcd5929a
zain abbas <zainabbaskhakhi123@gmail.com>
2024-09-01 14:35:07 +0500

    Set executable permissions on build.sh

--- server/build.sh
+++ server/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Exit immediately if a command exits with a non-zero status
+# Exit immediately if a command exits with a non-zero status...
 set -e
 
 echo "Installing dependencies..."
