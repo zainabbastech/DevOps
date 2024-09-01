b0855a439170cc98002f59aed83f2de7ce8ebd77
zain abbas <zainabbaskhakhi123@gmail.com>
2024-09-01 11:51:32 +0500

    apply cors in backend

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+// Apply CORS middleware
+app.use(cors());
 
 app.get("/api", (req, res) => {
   console.log("hey");
